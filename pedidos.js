cffa8e5b518f0f232e578dd1ad0b1e38483c0f15
Richas <ricardo.goncalves.vaz@escola.pr.gov.br>
2026-02-19 19:48:46 -0300

    Update pedidos.js

--- pedidos.js
+++ pedidos.js
@@ -1,6 +1,6 @@
 /*
   TAREFA (Aluno 1 - Pedidos)
-  - Adicione 2 pedidos novos com id, customer, total e status.
+  - Adicione 2 pedidos novos com id, customer, total e status. (Feito)
   - Inclua um campo "delivery" (ex: "Retirada" ou "Entrega") em cada pedido.
   - Atualize um pedido existente para status "Cancelado".
   - Mantenha os valores como strings para facilitar a edicao.
@@ -25,6 +25,18 @@ window.AppData.orders = [
     id: "P-1003",
     customer: "Carlos Medeiros",
     total: "R$ 212,40",
+    status: "Enviado",
+  },
+   {
+    id: "P-1004",
+    customer: "Janaina",
+    total: "R$ 80,00",
+    status: "Pendente",
+  },
+  {
+    id: "P-1005",
+    customer: "Pablo",
+    total: "R$ 210,50",
     status: "Enviado",
   },
 ];
